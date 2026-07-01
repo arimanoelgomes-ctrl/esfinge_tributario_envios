42602cd6767a9351972e51d6b4a7c1ae953a8b75
Ari Manoel Gomes <arimanoel.gomes@betha.com.br>
2026-06-30 21:24:42 -0300

    chore: apontar dashboard para nova URL do Apps Script Web App
    A implantacao anterior (AKfycbxR...) ficou desatualizada porque foi
    criada uma implantacao nova (AKfycbwl...) em vez de atualizar a
    existente com Nova versao. O dashboard continuava chamando a URL
    antiga, que servia codigo sem tab de 05/2026.

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
  * Para atualizar, editar apps_script/Code.gs e reimplantar (Implantar > Gerenciar
  * implantações > editar versão existente > Nova versão > Implantar).
  */
-window.APPS_SCRIPT_URL = "https://script.google.com/a/macros/betha.com.br/s/AKfycbxRAwKnS-L2CAMoh-vJqZSHBfLrrTgjO8ukjcZfYN2enyDVQOjsSeARpIiZ8wZ8y7YY/exec";
+window.APPS_SCRIPT_URL = "https://script.google.com/a/macros/betha.com.br/s/AKfycbwlKRbC7gH9aMqPhBNQFIUzd-QRbrc7rftLz-5zYJjsozUK3GLnqdGaw3oAr5q19LQb/exec";
